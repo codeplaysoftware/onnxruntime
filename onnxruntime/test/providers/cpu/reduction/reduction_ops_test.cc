codeplaysoftware/onnxruntime

commit 8be16f70223cb3fd5e7a95480b1f062bffb3c910

Improves ReduceSum performance by removing transposition. (#5370)
* Improves ReduceSum performance
* Add min, max, L1, L2, logsum, sumsquare
* remove all reduce implementation including transpose

onnxruntime/core/providers/cpu/reduction/reduction_ops.h
@@ -10,24 +10,305 @@
 #include "core/providers/cpu/containers.h"
 #include "core/util/math_cpuonly.h"
 #include "core/platform/threadpool.h"
+#include <cmath>
 
 namespace onnxruntime {
 
-template <typename T>
-bool PrepareForReduce(const Tensor* input_tensor_ptr,
-                      FastAllocVector<T>& transposed_input_data,
-                      int64_t& block_size,
-                      int64_t& blocks,
-                      const std::vector<int64_t>& axes_,
-                      bool keepdims_,
-                      /*out*/ std::vector<int64_t>& reduced_dims,
-                      bool check_no_transpose = false,
-                      const TensorShape* input_shape_override = nullptr);
+class ResultsNoTransposePrepareForReduce {
+ public:
+  std::vector<int64_t> input_shape;
+  std::vector<int64_t> reduced_axes;
+  std::vector<int64_t> projected_index;
+  int64_t last_loop_red_size;
+  int64_t last_loop_red_inc;
+  std::vector<int64_t> unprojected_index;
+  int64_t last_loop_size;
+  int64_t last_loop_inc;
+  bool equal(const std::vector<int64_t>& local_input_shape, const std::vector<int64_t>& local_reduced_axes) {
+    if (input_shape.size() != local_input_shape.size())
+      return false;
+    if (reduced_axes.size() != local_reduced_axes.size())
+      return false;
+    for (std::vector<int64_t>::const_iterator it1 = input_shape.begin(), it2 = local_input_shape.begin();
+         it1 != input_shape.end(); ++it1, ++it2) {
+      if (*it1 != *it2)
+        return false;
+    }
+    for (std::vector<int64_t>::const_iterator it1 = reduced_axes.begin(), it2 = local_reduced_axes.begin();
+         it1 != reduced_axes.end(); ++it1, ++it2) {
+      if (*it1 != *it2)
+        return false;
+    }
+    return true;
+  }
+};
 
 template <typename T>
-void ReduceSumCore(const T* input_data, T* output_data, bool no_transpose,
-                   int64_t blocks, int64_t block_size, FastAllocVector<T>& transposed_input_data,
-                   concurrency::ThreadPool* tp);
+inline T reduce_sqrt(T value) { return std::sqrt(value); }
+
+template <>
+inline int64_t reduce_sqrt<int64_t>(int64_t value) { return static_cast<int64_t>(std::sqrt(static_cast<double>(value))); }
+
+template <>
+inline int32_t reduce_sqrt<int32_t>(int32_t value) { return static_cast<int32_t>(std::sqrt(static_cast<double>(value))); }
+
+template <typename T>
+inline T reduce_log(T value) { return static_cast<T>(std::log(value)); }
+
+template <>
+inline int64_t reduce_log<int64_t>(int64_t value) { return static_cast<int64_t>(std::log(static_cast<double>(value))); }
+
+template <>
+inline int32_t reduce_log<int32_t>(int32_t value) { return static_cast<int32_t>(std::log(static_cast<double>(value))); }
+
+template <typename T>
+inline T reduce_exp(T value) { return static_cast<T>(std::exp(value)); }
+
+template <typename T, typename TVAL = T>
+class ReduceAggregator {
+ public:
+  typedef TVAL value_type;
+
+ protected:
+  int64_t N_;
+  T accumulator_;
+
+ public:
+  inline ReduceAggregator(int64_t N, const T& init) {
+    N_ = N;
+    accumulator_ = init;
+  }
+  inline void update(const T&) { ORT_ENFORCE(false, "must be overloaded."); }
+  inline void update0(const T&) { ORT_ENFORCE(false, "must be overloaded."); }
+  inline TVAL aggall(const T*) { ORT_ENFORCE(false, "must be overloaded."); }
+  inline TVAL get_value() { return accumulator_; }
+  inline void enforce(const ResultsNoTransposePrepareForReduce&) {}
+  static inline bool two_loops() { return false; }
+};
+
+template <typename T, typename TVAL = T>
+class ReduceAggregatorSum : public ReduceAggregator<T, TVAL> {
+ public:
+  inline ReduceAggregatorSum(int64_t N, const T&) : ReduceAggregator<T, TVAL>(N, 0) {}
+  inline void update(const T& v) { this->accumulator_ += v; }
+  inline TVAL aggall(const T* from_data) {
+    return Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, 1>>(from_data, this->N_).sum();
+  }
+};
+
+template <typename T, typename TVAL = T>
+class ReduceAggregatorSumSquare : public ReduceAggregator<T, TVAL> {
+ public:
+  inline ReduceAggregatorSumSquare(int64_t N, const T&) : ReduceAggregator<T, TVAL>(N, 0) {}
+  inline TVAL aggall(const T* from_data) {
+    return Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, 1>>(from_data, this->N_).squaredNorm();
+  }
+  inline void update(const T& v) { this->accumulator_ += v * v; }
+};
+
+template <typename T, typename TVAL = T>
+class ReduceAggregatorMean : public ReduceAggregatorSum<T, TVAL> {
+ public:
+  inline ReduceAggregatorMean(int64_t N, const T&) : ReduceAggregatorSum<T, TVAL>(N, 0) {}
+  inline T aggall(const T* from_data) {
+    return Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, 1>>(from_data, this->N_).mean();
+  }
+  inline T get_value() { return this->accumulator_ / static_cast<T>(this->N_); }
+};
+
+template <typename T, typename TVAL = T>
+class ReduceAggregatorMax : public ReduceAggregator<T, TVAL> {
+ public:
+  inline ReduceAggregatorMax(int64_t N, const T& init) : ReduceAggregator<T, TVAL>(N, init) {}
+  inline TVAL aggall(const T* from_data) {
+    return Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, 1>>(from_data, this->N_).maxCoeff();
+  }
+  inline void update(const T& v) { this->accumulator_ = v > this->accumulator_ ? v : this->accumulator_; }
+};
+
+template <typename T, typename TVAL = int64_t>
+class ReduceAggregatorArgMinMax : public ReduceAggregator<T, TVAL> {
+ protected:
+  int64_t arg_;
+  int64_t index_;
+
+ public:
+  inline ReduceAggregatorArgMinMax(int64_t N, const T& init) : ReduceAggregator<T, TVAL>(N, init) {
+    arg_ = 0;
+    index_ = 0;
+  }
+  inline TVAL get_value() { return arg_; }
+  inline void enforce(const ResultsNoTransposePrepareForReduce& res) {
+    ORT_ENFORCE(res.projected_index.size() == 0, "Only one axis is allowed for reduction.");
+  }
+};
+
+template <typename T, typename TVAL = int64_t>
+class ReduceAggregatorArgMax : public ReduceAggregatorArgMinMax<T, TVAL> {
+ public:
+  inline ReduceAggregatorArgMax(int64_t N, const T& init) : ReduceAggregatorArgMinMax<T, TVAL>(N, init) {}
+  inline TVAL aggall(const T* from_data) {
+    Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, 1>>(from_data, this->N_).maxCoeff(&this->arg_);
+    return this->get_value();
+  }
+  inline void update(const T& v) {
+    if (v > this->accumulator_) {
+      this->accumulator_ = v;
+      this->arg_ = this->index_;
+    }
+    ++this->index_;
+  }
+};
+
+template <typename T, typename TVAL = int64_t>
+class ReduceAggregatorArgMaxLastIndex : public ReduceAggregatorArgMax<T, TVAL> {
+ public:
+  inline ReduceAggregatorArgMaxLastIndex(int64_t N, const T& init) : ReduceAggregatorArgMax<T, TVAL>(N, init) {}
+  inline TVAL aggall(const T* from_data) {
+    for (int64_t i = 0; i < this->N_; ++i) {
+      update(from_data[i]);
+    }
+    return this->get_value();
+  }
+  inline void update(const T& v) {
+    if (v >= this->accumulator_) {
+      this->accumulator_ = v;
+      this->arg_ = this->index_;
+    }
+    ++this->index_;
+  }
+};
+
+template <typename T, typename TVAL = int64_t>
+class ReduceAggregatorArgMin : public ReduceAggregatorArgMinMax<T, TVAL> {
+ public:
+  inline ReduceAggregatorArgMin(int64_t N, const T& init) : ReduceAggregatorArgMinMax<T, TVAL>(N, init) {}
+  inline TVAL aggall(const T* from_data) {
+    Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, 1>>(from_data, this->N_).minCoeff(&this->arg_);
+    return this->get_value();
+  }
+  inline void update(const T& v) {
+    if (v < this->accumulator_) {
+      this->accumulator_ = v;
+      this->arg_ = this->index_;
+    }
+    ++this->index_;
+  }
+};
+
+template <typename T, typename TVAL = int64_t>
+class ReduceAggregatorArgMinLastIndex : public ReduceAggregatorArgMin<T, TVAL> {
+ public:
+  inline ReduceAggregatorArgMinLastIndex(int64_t N, const T& init) : ReduceAggregatorArgMin<T, TVAL>(N, init) {}
+  inline TVAL aggall(const T* from_data) {
+    for (int64_t i = 0; i < this->N_; ++i) {
+      update(from_data[i]);
+    }
+    return this->get_value();
+  }
+  inline void update(const T& v) {
+    if (v <= this->accumulator_) {
+      this->accumulator_ = v;
+      this->arg_ = this->index_;
+    }
+    ++this->index_;
+  }
+};
+
+template <typename T, typename TVAL = T>
+class ReduceAggregatorMin : public ReduceAggregator<T, TVAL> {
+ public:
+  inline ReduceAggregatorMin(int64_t N, const T& init) : ReduceAggregator<T, TVAL>(N, init) {}
+  inline TVAL aggall(const T* from_data) {
+    return Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, 1>>(from_data, this->N_).minCoeff();
+  }
+  inline void update(const T& v) { this->accumulator_ = v < this->accumulator_ ? v : this->accumulator_; }
+};
+
+template <typename T, typename TVAL = T>
+class ReduceAggregatorProd : public ReduceAggregator<T, TVAL> {
+ public:
+  inline ReduceAggregatorProd(int64_t N, const T&) : ReduceAggregator<T, TVAL>(N, 1) {}
+  inline TVAL aggall(const T* from_data) {
+    return Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, 1>>(from_data, this->N_).prod();
+  }
+  inline void update(const T& v) { this->accumulator_ *= v; }
+};
+
+template <typename T, typename TVAL = T>
+class ReduceAggregatorL1 : public ReduceAggregator<T, TVAL> {
+ public:
+  inline ReduceAggregatorL1(int64_t N, const T&) : ReduceAggregator<T, TVAL>(N, 0) {}
+  inline TVAL aggall(const T* from_data) {
+    return Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, 1>>(from_data, this->N_).cwiseAbs().sum();
+  }
+  inline void update(const T& v) { this->accumulator_ += v > 0 ? v : -v; }
+};
+
+template <typename T, typename TVAL = T>
+class ReduceAggregatorL2 : public ReduceAggregator<T, TVAL> {
+ public:
+  inline ReduceAggregatorL2(int64_t N, const T&) : ReduceAggregator<T, TVAL>(N, 0) {}
+  inline TVAL aggall(const T* from_data) {
+    return Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, 1>>(from_data, this->N_).norm();
+  }
+  inline void update(const T& v) { this->accumulator_ += v * v; }
+  inline TVAL get_value() { return reduce_sqrt<T>(this->accumulator_); }
+};
+
+template <typename T, typename TVAL = T>
+class ReduceAggregatorLogSum : public ReduceAggregator<T, TVAL> {
+ public:
+  inline ReduceAggregatorLogSum(int64_t N, const T&) : ReduceAggregator<T, TVAL>(N, 0) {}
+  inline T aggall(const T* from_data) {
+    return reduce_log<T>(Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, 1>>(from_data, this->N_).sum());
+  }
+  inline void update(const T& v) { this->accumulator_ += v; }
+  inline TVAL get_value() { return reduce_log<T>(this->accumulator_); }
+};
+
+template <typename T, typename TVAL = T>
+class ReduceAggregatorLogSumExp : public ReduceAggregator<T, TVAL> {
+ protected:
+  T max_;
+
+ public:
+  inline ReduceAggregatorLogSumExp(int64_t N, const T&) : ReduceAggregator<T, TVAL>(N, 0) { max_ = this->accumulator_; }
+  inline TVAL aggall(const T* from_data) {
+    max_ = Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, 1>>(from_data, this->N_).maxCoeff();
+    for (int64_t i = 0; i < this->N_; ++i) {
+      update(from_data[i]);
+    }
+    return get_value();
+  }
+  inline void update0(const T& v) { max_ = v > max_ ? v : max_; }
+  inline void update(const T& v) { this->accumulator_ += reduce_exp(v - max_); }
+  inline TVAL get_value() { return reduce_log<T>(this->accumulator_) + max_; }
+  static inline bool two_loops() { return true; }
+};
+
+bool SetupForReduce(const Tensor* input_tensor_ptr,
+                    const std::vector<int64_t>& axes_,
+                    std::vector<int64_t>& axes,
+                    TensorShape& new_input_shape,
+                    std::vector<int64_t>& output_shape,
+                    bool& empty_reduce,
+                    const TensorShape* input_shape_override);
+
+void NoTransposePrepareForReduce(const TensorShape& new_input_shape,
+                                 const std::vector<int64_t>& reduced_axes,
+                                 ResultsNoTransposePrepareForReduce& results);
+
+template <typename T, typename AGG>
+void NoTransposeReduce(Tensor* output, const TensorShape& new_input_shape, const Tensor& input,
+                       const std::vector<int64_t>& reduced_axes, concurrency::ThreadPool* tp,
+                       ResultsNoTransposePrepareForReduce& last_results);
+
+template <typename T, typename AGG>
+void CommonReduce(OpKernelContext* ctx,
+                  const std::vector<int64_t> axes_, int64_t keepdims_,
+                  ResultsNoTransposePrepareForReduce& last_results,
+                  bool noop_with_empty_axes = false);
 
 template <bool allow_multi_axes>
 class ReduceKernelBase {

orttraining/orttraining/training_ops/cpu/reduction/reduction_ops.cc
@@ -27,38 +27,10 @@ REGISTER_REDUCESUMTRAINING_KERNEL_TYPED(double)
 REGISTER_REDUCESUMTRAINING_KERNEL_TYPED(int32_t)
 REGISTER_REDUCESUMTRAINING_KERNEL_TYPED(int64_t)
 
-
 template <typename T>
 Status ReduceSumTraining<T>::Compute(OpKernelContext* ctx) const {
-  FastAllocVector<T> transposed_input_data(GetAllocator<T>(*ctx));
-  int64_t block_size;
-  int64_t blocks;
-  std::vector<int64_t> reduced_dims;
-  const Tensor* input = ctx->Input<Tensor>(0);
-
-  //override the attribute value with the input value for reduction_axes
-  const Tensor* axes_tensor = ctx->Input<Tensor>(1);
-  ORT_ENFORCE(axes_tensor != nullptr, "Axes input is null");
-  ORT_ENFORCE(axes_tensor->Shape().NumDimensions() == 1,
-              "An axes tensor must be a vector tensor.");
-  auto nDims = static_cast<size_t>(axes_tensor->Shape()[0]);
-  const auto* data = axes_tensor->template Data<int64_t>();
-  std::vector<int64_t> axes(data, data + nDims);
-
-  // empty axes and no-op
-  if (axes.empty() && noop_with_empty_axes_) {
-    auto* output = ctx->Output(0, input->Shape());
-    memcpy(output->template MutableData<T>(), input->template Data<T>(), input->SizeInBytes());
-    return Status::OK();
-  }
-
-  bool no_transpose = PrepareForReduce<T>(input, transposed_input_data, block_size, blocks, axes, keepdims_, reduced_dims, true);
-
-  auto* output = ctx->Output(0, reduced_dims);
-
-  ReduceSumCore(input->template Data<T>(), output->template MutableData<T>(),
-                no_transpose, blocks, block_size, transposed_input_data, ctx->GetOperatorThreadPool());
-
+  ResultsNoTransposePrepareForReduce last_results;
+  CommonReduce<T, ReduceAggregatorSum<T>>(ctx, axes_, keepdims_, last_results, noop_with_empty_axes_);
   return Status::OK();
 }
 

commit 3f382eb5b6be931ee71229d356a4976be5a361a9

Handle -inf in ReduceSumLogExp, fix regression introduced in PR #5370 (#5583)
* Handle -inf in ReduceSumLogExp operator
* Update reduction_ops_test.cc
* Remove a case which has a different behaviour CPU/GPU

onnxruntime/core/providers/cpu/reduction/reduction_ops.h
@@ -64,6 +64,36 @@ inline int32_t reduce_log<int32_t>(int32_t value) { return static_cast<int32_t>(
 template <typename T>
 inline T reduce_exp(T value) { return static_cast<T>(std::exp(value)); }
 
+template <typename T>
+inline bool reduce_isinf(T value) { return std::isinf(value); }
+
+template <>
+inline bool reduce_isinf<int8_t>(int8_t) { return false; }
+
+template <>
+inline bool reduce_isinf<uint8_t>(uint8_t) { return false; }
+
+template <>
+inline bool reduce_isinf<int32_t>(int32_t) { return false; }
+
+template <>
+inline bool reduce_isinf<int64_t>(int64_t) { return false; }
+
+template <typename T>
+inline bool reduce_isnan(T value) { return std::isnan(value); }
+
+template <>
+inline bool reduce_isnan<int8_t>(int8_t) { return false; }
+
+template <>
+inline bool reduce_isnan<uint8_t>(uint8_t) { return false; }
+
+template <>
+inline bool reduce_isnan<int32_t>(int32_t) { return false; }
+
+template <>
+inline bool reduce_isnan<int64_t>(int64_t) { return false; }
+
 template <typename T, typename TVAL = T>
 class ReduceAggregator {
  public:
@@ -273,7 +303,9 @@ class ReduceAggregatorLogSumExp : public ReduceAggregator<T, TVAL> {
   T max_;
 
  public:
-  inline ReduceAggregatorLogSumExp(int64_t N, const T&) : ReduceAggregator<T, TVAL>(N, 0) { max_ = this->accumulator_; }
+  inline ReduceAggregatorLogSumExp(int64_t N, const T& init) : ReduceAggregator<T, TVAL>(N, 0) {
+    max_ = reduce_isinf(init) ? this->accumulator_ : init;
+  }
   inline TVAL aggall(const T* from_data) {
     max_ = Eigen::Map<const Eigen::Matrix<T, Eigen::Dynamic, 1>>(from_data, this->N_).maxCoeff();
     for (int64_t i = 0; i < this->N_; ++i) {
@@ -281,7 +313,9 @@ class ReduceAggregatorLogSumExp : public ReduceAggregator<T, TVAL> {
     }
     return get_value();
   }
-  inline void update0(const T& v) { max_ = v > max_ ? v : max_; }
+  inline void update0(const T& v) {
+    max_ = (reduce_isinf(v) || reduce_isnan(v) || v < max_) ? max_ : v;
+  }
   inline void update(const T& v) { this->accumulator_ += reduce_exp(v - max_); }
   inline TVAL get_value() { return reduce_log<T>(this->accumulator_) + max_; }
   static inline bool two_loops() { return true; }

onnxruntime/test/providers/cpu/reduction/reduction_ops_test.cc
@@ -15,6 +15,9 @@
 namespace onnxruntime {
 namespace test {
 
+const float FLOAT_INF = std::numeric_limits<float>::infinity();
+const float FLOAT_NINF = -std::numeric_limits<float>::infinity();
+
 // Disable TensorRT on some of the tests because the limit in its parser: axis >=0 && axis < nbDims
 template <typename OutT>
 void TestReduceOp(const std::string& op,
@@ -2016,6 +2019,87 @@ TEST(ReductionOpTest, ReduceDimWithZero) {
   test3.AddInput<float>("data", {3, 0, 2}, {});
   test3.AddOutput<float>("reduced", {3, 0}, {});
   run(test3);
+}
+
+TEST(ReductionOpTest, ReduceInfMax) {
+  OpTester test("ReduceMax");
+  test.AddAttribute("axes", std::vector<int64_t>{1});
+  test.AddAttribute("keepdims", (int64_t)0);
+  test.AddInput<float>("data", {6, 2},
+                       {1.0f, FLOAT_NINF,
+                        FLOAT_NINF, 4.0f,
+                        FLOAT_INF, FLOAT_NINF,
+                        FLOAT_NINF, FLOAT_INF,
+                        1.0f, FLOAT_INF,
+                        FLOAT_INF, 4.0f});
+  test.AddOutput<float>("reduced", {6},
+                        {1.0f, 4.0f,
+                         FLOAT_INF, FLOAT_INF,
+                         FLOAT_INF, FLOAT_INF});
+  test.Run();
+}
+
+TEST(ReductionOpTest, ReduceInfMin) {
+  OpTester test("ReduceMin");
+  test.AddAttribute("axes", std::vector<int64_t>{1});
+  test.AddAttribute("keepdims", (int64_t)0);
+  test.AddInput<float>("data", {6, 2},
+                       {1.0f, FLOAT_INF,
+                        FLOAT_INF, 4.0f,
+                        FLOAT_INF, FLOAT_NINF,
+                        FLOAT_NINF, FLOAT_INF,
+                        1.0f, FLOAT_NINF,
+                        FLOAT_NINF, 4.0f});
+  test.AddOutput<float>("reduced", {6},
+                        {1.0f, 4.0f,
+                         FLOAT_NINF, FLOAT_NINF,
+                         FLOAT_NINF, FLOAT_NINF});
+  test.Run();
+}
+
+TEST(ReductionOpTest, ReduceInfSum) {
+  OpTester test("ReduceSum");
+  test.AddAttribute("axes", std::vector<int64_t>{1});
+  test.AddAttribute("keepdims", (int64_t)0);
+  test.AddInput<float>("data", {6, 2},
+                       {1.0f, FLOAT_INF,
+                        FLOAT_INF, 4.0f,
+                        FLOAT_INF, FLOAT_NINF,
+                        FLOAT_NINF, FLOAT_INF,
+                        1.0f, FLOAT_NINF,
+                        FLOAT_NINF, 4.0f});
+  test.AddOutput<float>("reduced", {6},
+                        {FLOAT_INF, FLOAT_INF,
+                         std::numeric_limits<float>::quiet_NaN(), std::numeric_limits<float>::quiet_NaN(),
+                         FLOAT_NINF, FLOAT_NINF});
+  test.Run();
+}
+
+TEST(ReductionOpTest, ReduceInfLogSum) {
+  OpTester test("ReduceLogSum");
+  test.AddAttribute("axes", std::vector<int64_t>{1});
+  test.AddAttribute("keepdims", (int64_t)0);
+  test.AddInput<float>("data", {6, 2},
+                       {1.0f, FLOAT_INF,
+                        FLOAT_INF, 1.0f,
+                        FLOAT_INF, FLOAT_NINF,
+                        FLOAT_NINF, FLOAT_INF,
+                        1.0f, FLOAT_NINF,
+                        FLOAT_NINF, 1.0f});
+  test.AddOutput<float>("reduced", {6},
+                        {FLOAT_INF, FLOAT_INF,
+                         -std::numeric_limits<float>::quiet_NaN(), std::numeric_limits<float>::quiet_NaN(),
+                         std::numeric_limits<float>::quiet_NaN(), std::numeric_limits<float>::quiet_NaN()});
+  test.Run();
+}
+
+TEST(ReductionOpTest, ReduceInfLogSumExp) {
+  OpTester test("ReduceLogSumExp");
+  test.AddAttribute("axes", std::vector<int64_t>{1});
+  test.AddAttribute("keepdims", (int64_t)0);
+  test.AddInput<float>("data", {2, 2}, {1.0f, FLOAT_NINF, FLOAT_NINF, 1.0f});
+  test.AddOutput<float>("reduced", {2}, {1.0f, 1.0f});
+  test.Run();
 }
 
 }  // namespace test