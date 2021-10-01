162cec8cd6fc776156241805b7988879dc81f29f
Ouadie EL FAROUKI <ouadie.elfarouki@codeplay.com>
2021-10-01 18:00:01 +0100

    Minimal source/header/txt files settings for SYCL EP enabled build

---
+++ include/onnxruntime/core/providers/sycl/sycl_provider_factory.h
@@ -0,0 +1,13 @@
+// Codeplay Software Ltd.
+
+#include "onnxruntime_c_api.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+ORT_API_STATUS(OrtSessionOptionsAppendExecutionProvider_SYCL, _In_ OrtSessionOptions* options);
+
+#ifdef __cplusplus
+}
+#endif
